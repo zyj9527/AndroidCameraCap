783887a19aa06a85066a49fe7fc8a9d95e51f03d
tanghongliang <tanghongliang@citos.cn>
2017-09-28 09:31:05 +0800

    modify Android.mk

--- Android.mk
+++ Android.mk
@@ -11,8 +11,6 @@ LOCAL_SRC_FILES:= \
 	resource_manager.cpp
 
 LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES -DBUILD_FOR_ANDROID -DGLM_FORCE_CXX98
-#LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES -DBUILD_FOR_ANDROID -DGLM_FORCE_CXX98 -DGLM_MESSAGES
-#LOCAL_CPPFLAGS += -I$(LOCAL_PATH)/glm -std=c++11 -Wall -fno-exceptions -fno-rtti
 
 LOCAL_SHARED_LIBRARIES := \
 	libcutils \
@@ -26,22 +24,13 @@ LOCAL_SHARED_LIBRARIES := \
     libEGL \
     libGLESv2 \
     libgui
-#    libGLESv1_CM
 
 LOCAL_C_INCLUDES := \
 	bionic \
-	bionic/libstdc++/include \
 	external/stlport/stlport
 
 LOCAL_C_INCLUDES += \
-	$(call include-path-for, corecg graphics)
-
-LOCAL_C_INCLUDES += \
-    frameworks/base/include/ui \
-    frameworks/base/include/surfaceflinger \
-    frameworks/base/include/camera \
-    frameworks/base/include/media \
-	$(LOCAL_PATH)
+	$(call include-path-for, corecg graphics libstdc++)
 
 LOCAL_NDK_STL_VARIANT := stlport_shared
 	
